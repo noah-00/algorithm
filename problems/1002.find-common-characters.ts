1a04b945ab0ab6c78a68b7ef462fb0b50f375970
noah-00 <t71.ishizaka.naoya@gmail.com>
2024-03-13 08:49:52 +0900

    1002.find-common-characters

---
+++ problems/1002.find-common-characters.ts
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=1002 lang=typescript
+ *
+ * [1002] Find Common Characters
+ */
+
+// @lc code=start
+function commonChars(words: string[]): string[] {
+  let wordArr = words[0].split(""); // "bella" => ["b", "e", "l", "l", "a"]
+
+  for (let i = 1; i < words.length; i++) {
+    const newCheck: string[] = [];
+
+    for (let j = 0; j < words[i].length; j++) {
+      if (wordArr.includes(words[i][j])) {
+        const index = wordArr.findIndex((x) => x === words[i][j]);
+        newCheck.push(words[i][j]);
+        wordArr.splice(index, 1);
+      }
+    }
+
+    wordArr = newCheck;
+  }
+
+  return wordArr;
+}
+// @lc code=end
