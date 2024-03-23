962dea04433c2576beef39458909702d12e48bde
noah-00 <t71.ishizaka.naoya@gmail.com>
2024-03-23 10:18:52 +0900

    144.binary-tree-preorder-traversal

---
+++ problems/144.binary-tree-preorder-traversal.ts
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=144 lang=typescript
+ *
+ * [144] Binary Tree Preorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function preorderTraversal(root: TreeNode | null): number[] {
+  if (!root) return [];
+  const result = [root.val];
+  result.push(...preorderTraversal(root?.left));
+  result.push(...preorderTraversal(root?.right));
+  return result;
+}
+// @lc code=end
