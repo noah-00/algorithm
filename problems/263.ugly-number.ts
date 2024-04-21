c7efbeecd9a64522dc0b0130f06286a3a0c7d938
noah-00 <t71.ishizaka.naoya@gmail.com>
2024-04-21 10:09:33 +0900

    263.ugly-number

---
+++ problems/263.ugly-number.ts
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=263 lang=typescript
+ *
+ * [263] Ugly Number
+ */
+
+// @lc code=start
+function isUgly(n: number): boolean {
+  if (n == 0) return false;
+  while (n % 2 == 0) n /= 2;
+  while (n % 3 == 0) n /= 3;
+  while (n % 5 == 0) n /= 5;
+  return n == 1;
+}
+// @lc code=end
