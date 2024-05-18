fdb6530758855d26f8d947963a42dce80dc96714
noah-00 <t71.ishizaka.naoya@gmail.com>
2024-05-18 09:25:36 +0900

    69.sqrt-x

---
+++ problems/69.sqrt-x.ts
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=69 lang=typescript
+ *
+ * [69] Sqrt(x)
+ */
+
+// @lc code=start
+function mySqrt(x: number): number {
+  let low = 0;
+
+  let high = x;
+
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+
+    const square = mid * mid;
+
+    if (square <= x) low = mid + 1;
+    else high = mid - 1;
+  }
+
+  return high;
+}
+// @lc code=end
