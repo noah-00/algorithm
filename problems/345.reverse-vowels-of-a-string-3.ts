c55a59b962c0c4a0d4f61e4437d6495c7a4705ab
noah-00 <t71.ishizaka.naoya@gmail.com>
2024-06-04 09:09:26 +0900

    345.reverse-vowels-of-a-string-3

---
+++ problems/345.reverse-vowels-of-a-string-3.ts
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=345 lang=typescript
+ *
+ * [345] Reverse Vowels of a String
+ */
+
+// @lc code=start
+function reverseVowels(s: string): string {
+  let start = 0;
+  let end = s.length - 1;
+
+  const VOWELS = new Set(["a", "i", "u", "e", "o", "A", "I", "U", "E", "O"]);
+  const ans = [...s];
+
+  while (start < end) {
+    if (!VOWELS.has(ans[start])) start++;
+    if (!VOWELS.has(ans[end])) end--;
+
+    // Swap vowels
+    if (VOWELS.has(ans[start]) && VOWELS.has(ans[end])) {
+      const temp = ans[start];
+      ans[start] = ans[end];
+      ans[end] = temp;
+      start++;
+      end--;
+    }
+  }
+  return ans.join("");
+}
+// @lc code=end
