9d0eca86b121b0aad71c2609939bed092625e456
noah-00 <t71.ishizaka.naoya@gmail.com>
2024-06-10 09:07:42 +0900

    problems/387.first-unique-character-in-a-string-3

---
+++ problems/387.first-unique-character-in-a-string-3.ts
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=387 lang=typescript
+ *
+ * [387] First Unique Character in a String
+ */
+
+// @lc code=start
+function firstUniqChar(s: string): number {
+  for (let i = 0; i < s.length; i++) {
+    if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) return i;
+  }
+  return -1;
+}
+// @lc code=end
