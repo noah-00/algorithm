407fe5b76aa61540d63277be365eeb638ffaab13
noah-00 <t71.ishizaka.naoya@gmail.com>
2024-07-19 09:43:25 +0900

    94.binary-tree-inorder-traversal

---
+++ problems/94.binary-tree-inorder-traversal.ts
@@ -0,0 +1,28 @@
+import { TreeNode } from "./Tree";
+
+/*
+ * @lc app=leetcode id=94 lang=typescript
+ *
+ * [94] Binary Tree Inorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function inorderTraversal(root: TreeNode | null): number[] {
+  if (root === null) return [];
+  return [...inorderTraversal(root.left), root.val, ...inorderTraversal(root.right)];
+}
+// @lc code=end
