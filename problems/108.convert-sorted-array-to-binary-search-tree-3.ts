465e44a9e0e3d04b1328dc91c2c092a2532f8740
noah-00 <t71.ishizaka.naoya@gmail.com>
2024-08-09 11:17:51 +0900

    108.convert-sorted-array-to-binary-search-tree-3

---
+++ problems/108.convert-sorted-array-to-binary-search-tree-3.ts
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=108 lang=typescript
+ *
+ * [108] Convert Sorted Array to Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+import { TreeNode } from "./Tree";
+
+function sortedArrayToBST(nums: number[]): TreeNode | null {
+  if (nums.length === 0) return null;
+
+  const centerIndex = Math.floor(nums.length / 2);
+  return new TreeNode(
+    nums[centerIndex],
+    sortedArrayToBST(nums.slice(0, centerIndex)),
+    sortedArrayToBST(nums.slice(centerIndex + 1))
+  );
+}
+// @lc code=end
