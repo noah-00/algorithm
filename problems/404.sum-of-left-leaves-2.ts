595f675daf2850e2eb3b3b24941ab4996d5c7613
noah-00 <t71.ishizaka.naoya@gmail.com>
2024-09-10 11:17:40 -0700

    404.sum-of-left-leaves-2

---
+++ problems/404.sum-of-left-leaves-2.ts
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode id=404 lang=typescript
+ *
+ * [404] Sum of Left Leaves
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+import { TreeNode } from "./Tree";
+
+function sumOfLeftLeaves(root: TreeNode | null): number {
+  let sum = 0;
+  function traverse(node: TreeNode | null) {
+    if (!node) return;
+
+    const left = node.left;
+    const right = node.right;
+
+    if (!left && !right) return;
+
+    if (left && !left.right && !left.left) {
+      // it is a left leaf
+      sum += left.val;
+    }
+
+    traverse(left);
+    traverse(right);
+  }
+
+  traverse(root);
+
+  return sum;
+}
+// @lc code=end
