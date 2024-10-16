d37115272e0d3459e1e1221bb850cf34fc4ae849
noah-00 <t71.ishizaka.naoya@gmail.com>
2024-10-16 09:20:28 -0700

    rm

--- problems/1.two-sum_3.ts
+++ problems/1.two-sum_3.ts
@@ -4,9 +4,6 @@
  * [1] Two Sum
  */
 
-// nums = [2, 7, 11, 15];
-// target = 9
-
 // @lc code=start
 function twoSum(nums: number[], target: number): number[] {
   const numMap: { [key: number]: number } = {};
@@ -15,16 +12,12 @@ function twoSum(nums: number[], target: number): number[] {
     const currentNum = nums[i];
 
     const complement = target - currentNum;
-    // 7
-    // 2
 
     if (complement in numMap) {
       return [numMap[complement], i];
-      // [0, 1]
     }
 
     numMap[currentNum] = i;
-    // { 2: 0 }
   }
 
   throw new Error("No solution found");
